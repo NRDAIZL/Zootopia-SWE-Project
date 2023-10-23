42e31412caeb6e67b68ebd74c9b1174159e21c68
Nadine <nadinehamdy128@gmail.com>
2023-10-23 20:29:01 +0300

    pet hotel adds

--- views/user/hotelbook.php
+++ views/user/hotelbook.php
@@ -7,49 +7,8 @@
     <link rel="stylesheet" href="../../public/css/calendar.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
-     <script>
-        $(document).ready(function() {
-    $("#datepicker-check-in").datepicker({
-        minDate: new Date(),
-        maxDate: "+1y",
-        dateFormat: "mm/dd/yy"
-    });
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-    $("#datepicker-check-out").datepicker({
-        minDate: new Date(),
-        maxDate: "+1y",
-        dateFormat: "mm/dd/yy"
-    });
-
-    // Rest of your code remains the same...
-});
-
-                     // Update the number of nights input field with the calculated number of nights
-// Calculate the number of nights between the two selected dates
-            function calculateNights(checkInDate, checkOutDate) {
-                const oneDay = 1000 * 60 * 60 * 24;
-                const diffInDays = (checkOutDate - checkInDate) / oneDay;
-                return Math.floor(diffInDays);
-            }
-
-
-            function updateNumberOfNights() {
-                const checkInDate = $("#datepicker-check-in").datepicker("getDate");
-                const checkOutDate = $("#datepicker-check-out").datepicker("getDate");
-                const numberOfNights = calculateNights(checkInDate, checkOutDate);
-                $("#number-of-nights-display").text(numberOfNights);
-            // Update the number of nights input field whenever the check-in or check-out date is changed
-            $("#datepicker-check-in").datepicker("option", "onSelect", function(date) {
-                updateNumberOfNights();
-            });
-
-            $("#datepicker-check-out").datepicker("option", "onSelect", function(date) {
-                updateNumberOfNights();
-            });
-
-   
-            }
-    </script> 
     <style>
         .booking-form {
             width: 500px;
@@ -79,15 +38,47 @@
 </head>
 
 <body>
-<?php include '../partials/menu.php';?>
+<script src="../../public/javascript/calendar.js"></script>
+
+<?php include '../partials/menu.php'; ?>
     <div class="booking-form">
         <h1>Pet Hotel Booking Form</h1>
+        <form>
+        <label>
+        <input type="radio" name="booking-type" value="daycare" checked>
+        Daycare
+    </label>
 
+    <label>
+        <input type="radio" name="booking-type" value="overnight">
+        Overnight
+    </label>
+    
         <input type="text" id="datepicker-check-in" placeholder="Check-in date">
-        <input type="text" id="datepicker-check-out" placeholder="Check-out date"> Number of nights: 
+        <input type="text" id="datepicker-check-out" placeholder="Check-out date">
+         <label for="number-of-nights-display"> Number of nights:</label>
+
         <div id="number-of-nights-display"></div>
+        <label for="animal-type">Animal Type:</label>
+<select id="animal-type" name="animal-type">
+    <option value="dog">Dog</option>
+    <option value="cat">Cat</option>
+    <option value="rabbit">Rabbit</option>
+    <option value="bird">Bird</option>
+    <option value="reptile">Reptile</option>
+    <option value="other">Other</option>
+</select>
+<br>
+        <label>
+        <input type="radio" name="gender" value="male"> Male
+    </label>
+    <label>
+        <input type="radio" name="gender" value="female"> Female
+    </label>
 
         <button type="submit">Book Now</button>
+        </form>
+        
     </div>
 </body>
 
