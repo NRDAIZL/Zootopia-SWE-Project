34cc05f8eb01a513b6913958142ef764a74b8a2f
alolastik <148634411+alolastik@users.noreply.github.com>
2023-10-24 19:22:04 +0300

    Add files via upload

---
+++ views/user/Home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Zootpia Home Page</title>
+    <link rel= "stylesheet" href="designHome.css"/>
+</head>
+
+<body>
+<h1>Zootopia! (hena el mafrood el nav bar)</h1>
+<hr>
+<?php
+session_start();
+//Checking if a user logged in or not.
+
+if(!empty($_SESSION['ID'])) {
+	echo "<h1>Welcome ".$_SESSION['FirstName']."</h1>";
+	//when mohammed does the profile viewer it will go here.
+}
+else{
+	echo "<h1>Welcome To Zootopia!</h1>";
+	
+    
+
+    echo"First time? " , "<a href='SignUp.php'>Signup Here.</a>" ;
+
+	
+    
+    echo"  Already a customer? " , "<a href='Login.php'>Login.</a>", "<hr>";
+	
+}
+?>
+<br><br>
+<button id="Services">Services</button>
+<br><br>
+<button id="Appoitments">Appoitments</button>
+<br><br>
+<button id="Host">Pet Hosting</button>
+<br><br>
+<button id="Vaccines" >Vaccines</button>
+
+
+</html>
---
+++ views/user/SignUp.php
@@ -0,0 +1,62 @@
+<?php
+  include_once "includes/DataBase.php";
+?>
+
+
+<!DOCTYPE html>
+<html>
+   <head>
+     <title>Sign Up</title>
+     <link rel= "stylesheet" href="designSignup.css"/>
+   </head>
+
+   <body>
+
+   <div id="form">
+    <h1 id="Title" style="font-family: Arial" >Sign Up</h1>
+   
+   <form action="" method="post">
+    <div id="formBoxes">
+   <label style="font-family: Arial">First Name:</label><br>
+  <input type="text" name="FirstName"><br>
+
+  <label style="font-family: Arial">Last Name:</label><br>
+  <input type="text" name="LastName"><br>
+
+  <label style="font-family: Arial">Email:</label><br>
+  <input type="text" name="Email"><br>
+
+  <label style="font-family: Arial">Password:</label><br>
+  <input type="Password" name="Password"><br>
+
+  <input style="font-family: Arial" type="submit" value="Submit" name="Submit">
+  <input style="font-family: Arial" type="reset">
+</div>
+</form>
+</div>
+<?php
+ //grap data from user if form was submitted 
+
+  if($_SERVER["REQUEST_METHOD"]=="POST"){ //check if form was submitted
+	$Fname=htmlspecialchars($_POST["FirstName"]);
+	$Lname=htmlspecialchars($_POST["LastName"]);
+	$Email=htmlspecialchars($_POST["Email"]);
+	$Password=htmlspecialchars($_POST["Password"]);
+	
+
+    //insert it to database 
+	$sql="insert into users(FirstName,LastName,Email,Password) 
+	values('$Fname','$Lname','$Email','$Password')";
+	$result=mysqli_query($conn,$sql);
+
+    //redirect the user back to index.php 
+	if($result)	{
+		header("Location:Home.php");
+	}
+}
+
+?>
+
+   </body>
+
+</html>
