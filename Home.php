38bb97e134840d5ab2f8783a2806065dba242e4a
alolastik <148634411+alolastik@users.noreply.github.com>
2023-10-24 16:23:31 +0300

    Create Home.php
    El homepage title and stuff

---
+++ Home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Zootpia Home Page</title>
+    <link rel= "stylesheet" href="designHome.css"/>
+</head>
+
+<body>
+<h1>Zootopia! (hena el mafrood el nav bar)</h1>
+<hr>
