3ed5ff74e2f2860b944b8deaccd0c37fc58b963b
Nadine <nadinehamdy128@gmail.com>
2023-10-25 15:25:51 +0300

    admin basics

---
+++ views/admin/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+// Include header
+include('../partials/admin-header.php');
+
+?>
+
+<main>
+
+
+
+
+</main>
+
+<?php
+
+// Include footer
+include('../partials/footer.php');
+
+?>
---
+++ views/partials/admin-header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>zootopia clinic</title>
+    <link rel="stylesheet" href="../../public/css/menu.css?v=<?php echo time(); ?>">
+    <script src="../../public/javascript/menu.js?v=<?php echo time(); ?>"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <nav>
+        <img src="../../public/images/logo.png" class="logo" alt="the clinic logo"></img>
+    
+        <ul>
+      <li><a href="../admin/dashboard.php">Dashboard</a></li>
+      <li><a href="admin.php?page=users">Users</a></li>
+      <li><a href="admin.php?page=products">Products</a></li>
+      <li><a href="#">Sign out</a></li>
+    </ul>
+        <label id="icon">
+           <i class="fas fa-bars"></i> 
+        </label>
+    </nav>
+</body>
+
+
+
+
+
+
+</html>
