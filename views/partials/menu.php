61da8278456703bdb67195a599b08433cd7a2c07
Nadine <nadinehamdy128@gmail.com>
2023-10-26 12:11:15 +0300

    links

--- views/partials/menu.php
+++ views/partials/menu.php
@@ -17,7 +17,7 @@
         <ul id="myElement" onclick="toggleHover()">
 
             <li><a class="ho" href="#">Home</a></li>
-            <li><a class="ho" href="#">Registration</a></li>
+            <li><a class="ho" href="../../views/user/SignUp.php">Registration</a></li>
             <li><a class="ho" href="../../views/user/appointment.php">Book an appointment</a></li>
             <li><a class="ho" href="#">Services</a></li>
             <li><a class="ho" href="../../views/user/viewprofile.php">Profile</a></li>
