c4cba5f9a44f3019b34e3ab16d53b0059cf43b36
sondos-abdelnasser <148705692+sondos-abdelnasser@users.noreply.github.com>
2023-11-01 12:06:31 +0200

    s

--- views/user/vaccine.php
+++ views/user/vaccine.php
@@ -4,7 +4,7 @@
         <meta charset ="UTF-8">
         <meta http-equiv ="X-UA- compatible compatible" content ="IE=edge">
         <meta name ="viewport" content="width=device-width,initial-scale=1.0">
-        <title>grooming </title>
+        <title>Vaccination </title>
 
 <!--font type-->
 <!--<link rel ="stylesheet" herf="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
