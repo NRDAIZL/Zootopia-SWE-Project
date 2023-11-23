8e9d9ed18bdd811681904746cc789775d2237fd0
Zainab <zainab2108676@miuegypt.edu.eg>
2023-11-23 14:44:25 +0200

    hi

---
+++ views/partials/footer.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>footer</title>
+        <meta charset="utf-8">
+        <meta name="viewport" contenr="width=device-width, initial-scale=1">
+       
+        <link rel="stylesheet" href="../../public/css/footer.css?v=<?php echo time(); ?>">
+    <script src="../../public/javascript/footer.js?v=<?php echo time(); ?>"></script>
+</head>
+<body>
+<nav>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>clinic working hours</h4>
+                <ul>
+                    <li>9 to 11</li>
+</ul>
+</div>
+
+</div>
+</div>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4></h4>
+                <ul>
+                    <li><a href="#"></a></li>
+</ul>
+</div>
+
+</div>
+</div>
+</footer>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4></h4>
+                <ul>
+                    <li><a href="#"></a></li>
+</ul>
+</div>
+
+</div>
+</div>
+</footer>
+
+</nav>
+
+
+
+
+
+
+
+
+</body>
+</html>
