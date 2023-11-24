a2ce9c6067f8b939c99e1aeab77a191ad26c6faf
Nadine <nadinehamdy128@gmail.com>
2023-11-24 12:14:02 +0200

    admin template almost ready

---
+++ views/admin/pethotel.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible"
+		content="IE=edge">
+	<meta name="viewport"
+		content="width=device-width, 
+				initial-scale=1.0">
+	<title>ZOOTOPIA</title> 
+	<link rel="stylesheet"
+		href="../../public/css/admin-main.css">
+	<link rel="stylesheet"
+		href="../../public/css/admin-res.css">
+        <script src="../../public/javascript/admin.js"></script>
+
+</head>
+
+<body>
+
+	<!-- for header part -->
+	<header>
+
+		<div class="logosec">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20221210182541/Untitled-design-(30).png"
+				class="icn menuicn"
+				id="menuicn"
+				alt="menu-icon">
+		</div>
+
+		<div class="searchbar">
+			<input type="text"
+				placeholder="Search">
+			<div class="searchbtn">
+			<img src="../../public/images/searchicon.png"
+
+
+					class="icn srchicn"
+					alt="search-icon">
+			</div>
+		</div>
+
+		<!-- <div class="message">
+			<div class="circle"></div>
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20221210183322/8.png"
+				class="icn"
+				alt="">
+			<div class="dp">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20221210180014/profile-removebg-preview.png"
+					class="dpicn"
+					alt="dp">
+			</div>
+		</div> -->
+
+	</header>
+
+	<div class="main-container">
+		<div class="navcontainer">
+        <?php include '../partials/admin-sidenav.php';?>
+
+		</div>
+		<div class="main">
+
+			<div class="searchbar2">
+				<input type="text"
+					name=""
+					id=""
+					placeholder="Search">
+				<div class="searchbtn">
+				<img src="../../public/images/searchicon.png"
+						class="icn srchicn"
+						alt="search-button">
+				</div>
+			</div>
+
+
+
+		</div>
+	</div>
+
+	<script src="./index.js"></script>
+</body>
+</html>
--- views/partials/admin-sidenav.php
+++ views/partials/admin-sidenav.php
@@ -1,74 +1,59 @@
-<!-- <head>    <link rel="stylesheet" href="../../public/css/menu.css?v=<?php echo time(); ?>">
-    <script src="../../public/javascript/menu.js?v=<?php echo time(); ?>"></script>
-</head>
-<style>
-.admin-sidebar {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 250px;
-  background-color: #fff;
-  padding: 20px;
-}
-
-.admin-sidebar ul {
-  list-style: none;
-  padding: 0;
-}
-
-.admin-sidebar li {
-  margin-bottom: 10px;
-}
-
-.admin-sidebar a {
-  display: block;
-  padding: 10px 0;
-  text-decoration: none;
-  color: #000;
-}
-
-.admin-sidebar a:hover {
-  background-color: #eee;
-  color: #333;
-}
-
-</style> -->
-<!-- <style>
-    .sidebar {
-  background-color: #f5f5f5;
-  padding: 20px;
-}
-
-.sidebar a {
-  color: #333;
-  text-decoration: none;
-}
-
-.sidebar a:hover {
-  color: #000;
-  text-decoration: underline;
-}
-
-.sidebar i {
-  margin-right: 10px;
-}
-
-</style> -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 
 <nav class="sidebar">
   <ul>
-    <li><a href="admin.php?page=dashboard"><i class="fa fa-tachometer"></i> Dashboard</a></li>
-    <li><a href="admin.php?page=clients"><i class="fa fa-paw"></i> Clients</a></li>
-    <li><a href="admin.php?page=pets"><i class="fa fa-heartbeat"></i> Pets</a></li>
-    <li><a href="admin.php?page=Appointments"><i class="fa fa-calendar"></i> Appointments</a></li>
-    <li><a href="admin.php?page=Inventory"><i class="fa fa-medkit"></i> Inventory</a></li>
-    <li><a href="admin.php?page=Financials"><i class="fa fa-money"></i> Financials</a></li>
-    <li><a href="admin.php?page=Settings"><i class="fa fa-cogs"></i> Settings</a></li>
-    <li><a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+    <li><a href="admin.php?page=dashboard"> Dashboard</a></li>
+    <li><a href="admin.php?page=schedule"> Availability</a></li>
+
+    <li><a href="admin.php?page=clients">Clients</a></li>
+    <li><a href="admin.php?page=pets"> Pets</a></li>
+    <li><a href="admin.php?page=Appointments">Appointments</a></li>
+    <li><a href="admin.php?page=hotel"> Hotel</a></li>
+    <li><a href="admin.php?page=Settings"> Settings</a></li>
+    <li><a href="logout.php"> Logout</a></li>
   </ul>
 </nav>
+<nav class="nav">
+				<div class="nav-upper-options">
+					<div class="nav-option option1">
+						<!-- <img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20221210182148/Untitled-design-(29).png"
+							class="nav-img"
+							alt="dashboard"> -->
+              <a href="admin.php?page=dashboard"><div class="link"> Dashboard</div></a>
+					</div>
+
+					<div class="option2 nav-option">
+						
+						<h3> Articles</h3>
+					</div>
+
+					<div class="nav-option option3">
+					
+						<h3> Report</h3>
+					</div>
+
+					<div class="nav-option option4">
+					
+						<h3> Institution</h3>
+					</div>
+
+					<div class="nav-option option5">
+						
+						<h3> Profile</h3>
+					</div>
+
+					<div class="nav-option option6">
+						
+						<h3> Settings</h3>
+					</div>
+
+					<div class="nav-option logout">
+						
+						<h3>Logout</h3>
+					</div>
+
+				</div>
+			</nav>
+
 
 
