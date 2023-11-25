e37f7f71463be2505592e6a5d9fd4565070d2734
Zainab <zainab2108676@miuegypt.edu.eg>
2023-11-26 00:38:46 +0200

    appointmentssadmin side m4 3rfa a3ml 7aga lsa

---
+++ views/admin/appointmentsas.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible"
+		content="IE=edge">
+	<meta name="viewport"
+		content="width=device-width, 
+				initial-scale=1.0">
+	<title>ZOOTOPIA</title> 
+	<link rel="stylesheet"
+		href="../../public/css/admin-main.css">
+	<link rel="stylesheet"
+		href="../../public/css/admin-res.css">
+        <link rel="stylesheet"
+		href="../../public/css/appointmentas.css">
+        <script src="../../public/javascript/admin.js"></script>
+        <script src="../../public/javascript/appointmentas.js"></script>
+
+</head>
+
+<body>
+
+	<!-- for header part -->
+	<header>
+
+	<?php include '../partials/admin-header.php';?>
+
+
+	</header>
+
+	<div class="main-container">
+		<div class="navcontainer">
+        <?php include '../partials/admin-sidenav.php';?>
+
+		</div>s
+		<div class="main">
+
+			<div class="searchbar2">
+				<input type="text"
+					name=""
+					id=""
+					placeholder="Search">
+				<div class="searchbtn">
+				<img src="../../public/images/searchicon.png"
+						class="icn srchicn"
+						alt="search-button">
+				</div>
+			</div>
+			
+
+
+
+		</div>
+	</div>
+
+	<script src="./index.js"></script>
+</body>
+</html>
