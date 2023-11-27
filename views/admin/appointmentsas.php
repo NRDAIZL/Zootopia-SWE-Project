47c3c004c1e29e43a83154c5bbf5fcf434037fd2
Zainab <zainab2108676@miuegypt.edu.eg>
2023-11-27 22:09:46 +0200

    b7awel a display el appointments table fil admin side bs t2riban m4 2ary eldatabase asln

--- views/admin/appointmentsas.php
+++ views/admin/appointmentsas.php
@@ -18,6 +18,10 @@
         <script src="../../public/javascript/admin.js"></script>
         <script src="../../public/javascript/appointmentas.js"></script>
 
+		<?php
+  include_once "../../config/dbh.inc.php";
+?>
+
 </head>
 
 <body>
@@ -56,5 +60,70 @@
 	</div>
 
 	<script src="./index.js"></script>
+
+<?php 
+
+$sql = "SELECT * FROM appointments";
+
+
+echo '<table border="0" cellspacing="2" cellpadding="2"> 
+      <tr> 
+          <td> <font face="Arial">pettype</font> </td> 
+          <td> <font face="Arial">petname</font> </td> 
+          <td> <font face="Arial">appointmentday</font> </td> 
+          <td> <font face="Arial">appointmenttime</font> </td> 
+      
+      </tr>';
+
+if ($result = $mysqli->query($sql)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["pettype"];
+        $field2name = $row["petname"];
+        $field3name = $row["appointmentday"];
+        $field4name = $row["appointmenttime"];
+
+
+		echo '<tr> 
+		<td>'.$field1name.'</td> 
+		<td>'.$field2name.'</td> 
+		<td>'.$field3name.'</td> 
+		<td>'.$field4name.'</td> 
+	
+	</tr>';
+        
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+
+
+  // Perform the query
+  try {
+	mysqli_query($conn, $sql);
+	echo "appointment registered successfully :)";
+
+} catch (Exception $e) {
+	echo "Error: " . $e->getMessage();
+} finally {
+	mysqli_close($conn);
+}
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
 </body>
 </html>
