87db0a5c216b78196b6025fdf5daa9ce6803a54b
Zainab <zainab2108676@miuegypt.edu.eg>
2023-11-27 22:28:57 +0200

    include template badal copy paste bs still elmenu btb2a foo2 elkalam

--- views/admin/appointmentsas.php
+++ views/admin/appointmentsas.php
@@ -3,19 +3,9 @@
 
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible"
-		content="IE=edge">
-	<meta name="viewport"
-		content="width=device-width, 
-				initial-scale=1.0">
-	<title>ZOOTOPIA</title> 
-	<link rel="stylesheet"
-		href="../../public/css/admin-main.css">
-	<link rel="stylesheet"
-		href="../../public/css/admin-res.css">
-        <link rel="stylesheet"
-		href="../../public/css/appointmentas.css">
-        <script src="../../public/javascript/admin.js"></script>
+	
+	<link rel="stylesheet" href="../../public/css/appointmentas.css">
+   
         <script src="../../public/javascript/appointmentas.js"></script>
 
 		<?php
@@ -29,37 +19,11 @@
 	<!-- for header part -->
 	<header>
 
-	<?php include '../partials/admin-header.php';?>
+	<?php include './templatefornewpage.php';?>
 
 
 	</header>
-
-	<div class="main-container">
-		<div class="navcontainer">
-        <?php include '../partials/admin-sidenav.php';?>
-
-		</div>s
-		<div class="main">
-
-			<div class="searchbar2">
-				<input type="text"
-					name=""
-					id=""
-					placeholder="Search">
-				<div class="searchbtn">
-				<img src="../../public/images/searchicon.png"
-						class="icn srchicn"
-						alt="search-button">
-				</div>
-			</div>
-			
-
-
-
-		</div>
-	</div>
-
-	<script src="./index.js"></script>
+	
 
 <?php 
 
@@ -101,16 +65,16 @@ if ($result = $mysqli->query($sql)) {
 
 
 
-  // Perform the query
-  try {
-	mysqli_query($conn, $sql);
-	echo "appointment registered successfully :)";
+//   // Perform the query
+//   try {
+// 	mysqli_query($conn, $sql);
+// 	echo "appointment registered successfully :)";
 
-} catch (Exception $e) {
-	echo "Error: " . $e->getMessage();
-} finally {
-	mysqli_close($conn);
-}
+// } catch (Exception $e) {
+// 	echo "Error: " . $e->getMessage();
+// } finally {
+// 	mysqli_close($conn);
+// }
 
 
 
