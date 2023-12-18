c60973f5113115c55ef682042bad1a8b79de6c24
sondos-abdelnasser <148705692+sondos-abdelnasser@users.noreply.github.com>
2023-12-18 03:02:58 +0200

    upload new vaccine admin side

---
+++ views/admin/uploadvac.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once '../../config/dbh.inc.php';
+
+if(isset($_POST["submit"])){
+  $vaccinename = $_POST["vaccinename"];
+  $price = $_POST["price"];
+  $vaccine_description = $_POST["vaccine_description"];
+
+  //For uploads photos
+  $upload_dir = "uploads/"; //this is where the uploaded photo stored
+  $vaccine_image = $upload_dir.$_FILES["imageUpload"]["name"];
+  $upload_dir.$_FILES["imageUpload"]["name"];
+  $upload_file = $upload_dir.basename($_FILES["imageUpload"]["name"]);
+  $imageType = strtolower(pathinfo($upload_file,PATHINFO_EXTENSION)); //used to detected the image format
+  $check = $_FILES["imageUpload"]["size"]; // to detect the size of the image
+  $upload_ok = 0;
+
+  if(file_exists($upload_file)){
+      echo "<script>alert('The file already exist')</script>";
+      $upload_ok = 0;
+  }else{
+      $upload_ok = 1;
+      if($check !== false){
+        $upload_ok = 1;
+        if($imageType == 'jpg' || $imageType == 'png' || $imageType == 'jpeg' || $imageType == 'gif'){
+            $upload_ok = 1;
+        }else{
+            echo '<script>alert("please change the image format")</script>';
+        }
+      }else{
+          echo '<script>alert("The photo size is 0 please change the photo ")</script>';
+          $upload_ok = 0;
+      }
+  }
+
+  if($upload_ok == 0){
+     echo '<script>alert("sorry your file is doesn\'t uploaded. Please try again")</script>';
+  }else{
+      if($vaccinename != "" && $price !=""&&$vaccine_description!=""){
+          move_uploaded_file($_FILES["imageUpload"]["tmp_name"],$upload_file);
+
+          $sql = "INSERT INTO vaccines(vaccine_name,price,VaccineDescription,vaccine_image)
+          VALUES('$vaccinename',$price,$vaccine_description,'$vaccine_image')";
+
+          if($conn->query($sql) === TRUE){
+              echo "<script>alert('your vaccine uploaded successfully')</script>";
+          }
+      }
+  }
+
+
+  
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="upload.css">
+</head>
+<body>
+    <?php
+    include_once 'header.php';
+    ?>
+     
+    <section id="upload_container">
+        <form action="upload.php" method="POST" enctype="multipart/form-data" >
+            <input type="text" name="vaccinename" id="VaccineName" placeholder="vaccine Name" required>
+            <input type="number" name="price" id="price" placeholder="vaccine Price" required>
+            <input type="number" name="discount" id="vaccine_description" placeholder="vaccine description">
+            <input type="file" name="imageUpload" id="imageUpload" required hidden>
+            <button id="choose" onclick="upload();">Choose Image</button>
+            <input type="submit" value="Upload" name="submit">
+        </form>
+    </section>
+
+    <script>
+        var vaccinename = document.getElementById("VaccineName");
+        var price = document.getElementById("price");
+        var vaccine_description = document.getElementById("vaccine_description");
+        var choose = document.getElementById("choose");
+        var uploadImage = document.getElementById("imageUpload");
+
+        function upload(){
+            uploadImage.click();
+        }
+
+        uploadImage.addEventListener("change",function(){
+            var file = this.files[0];
+            if(vaccinename.value == ""){
+                vaccinename.value = file.name;
+            }
+            choose.innerHTML = "You can change("+file.name+") picture";
+        })
+    </script>
+</body>
+</html>
--- views/user/vaccine.php
+++ views/user/vaccine.php
@@ -1,35 +1,50 @@
+<?php
+
+  require_once '../../config/dbh.inc.php';
+
+  $sql = "SELECT * FROM vaccines";
+  $all_vaccines = $conn->query($sql);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset ="UTF-8">
-        <meta http-equiv ="X-UA- compatible compatible" content ="IE=edge">
-        <meta name ="viewport" content="width=device-width,initial-scale=1.0">
-        <title>Vaccination </title>
-
-<!--font type-->
-<!--<link rel ="stylesheet" herf="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
-<link rel ="stylesheet"herf="css/style.css">-->
-<style type = "text/css">
-*{
-      margin: 0;
-    padding:0;
-     text-align: center;
-      font-family:arial;
-          transition: 0.5s;
-
-
-
-      }
-      body{
-        width: 100vw;
-        height: 100vh;
-        background:#c8e8e0 ;
-        padding-top: 50 px;
-
-      }
-</style>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>vaccination</title>
 </head>
-    <body>
-    </body>
+<body>
+    <?php
+
+    include_once 'header.php';
+    ?>
+
+   <main>
+       <?php
+          while($row = mysqli_fetch_assoc($all_vaccines)){
+       ?>
+       <div class="card">
+           <div class="image">
+               <img src="<?php echo $row["vaccine_image"]; ?>" alt="">
+           </div>
+           <div class="caption">
+               
+               <p class="vaccine_name"><?php echo $row["vaccine_name"];  ?></p>
+               <p class="price"><b>$<?php echo $row["price"]; ?></b></p>
+               <p class="vaccine-description"><b><del>$<?php echo $row[" VaccineDescription"]; ?></del></b></p>
+           </div>
+           <button class="book" data-id="<?php echo $row["vaccine_id"];  ?>">book now</button>
+       </div>
+       <?php
+
+          }
+     ?>
+   </main>
+   
+</body>
 </html>
